15c6b930be7eb95c052c97be2e471e87b2212cf3
pramation <pramation@yahoo.com>
2022-05-10 16:00:33 -0400

    Add files via upload

---
+++ deploy_app/run_kubernetes.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath="pramation/capstoneproj"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run capstoneprojmicroserviceapi\
+      --image=$dockerpath\
+      --port=8080 --labels app=capstoneprojmicroserviceapi
+
+
+sleep 30
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+sleep 30
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward capstoneprojmicroserviceapi 8080:8080
