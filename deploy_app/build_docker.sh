c3d44cb7af2f1e36bbcb072494a3090945cbb388
pramation <pramation@yahoo.com>
2022-05-10 17:30:36 -0400

    Create build_docker.sh

---
+++ deploy_app/build_docker.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build --tag=capstoneproj .
+
+# Step 2: 
+# List docker images
+docker image ls
