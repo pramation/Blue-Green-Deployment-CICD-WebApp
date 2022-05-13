ba9e2bcfb8d86a99088cc95ef2ab7e006a4bb447
pramation <pramation@yahoo.com>
2022-05-13 11:58:28 -0400

    Update app.py

--- deploy_app/app.py
+++ deploy_app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '==============================\n<br>Hello, World! This is Praveen\n<br>This is new Production<br>==============================\n<br>'
-Delete This  Error Line 
+#Delete This  Error Line 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
     
