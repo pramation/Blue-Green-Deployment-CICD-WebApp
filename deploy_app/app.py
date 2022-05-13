1a29ae90ef6008c52908c93597304a6e7065cbae
pramation <pramation@yahoo.com>
2022-05-12 20:09:42 -0400

    Update app.py

--- deploy_app/app.py
+++ deploy_app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '==============================\n<br>Hello, World! This is Praveen\n==============================\n<br>'
+    return '==============================\n<br>Hello, World! This is Praveen\n<br>This is new Production<br>==============================\n<br>'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
